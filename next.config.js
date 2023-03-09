7d330b79911d5ed05aeac64e65419c95697573cb
JJHaEun <yj05037@gmail.com>
2023-03-09 10:57:08 +0900

    분기처리

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   // swcMinify: true,
   trailingSlash: true,
+  generateBuildId: () => "codecamp-deploy-project",
+
+  // 아래의 주소들만 out폴더 만들어줘(getServerSideProps에 있는 페이지는 제외하기
+  exportPathMap: () => {
+    ({
+      "/": { page: "/" },
+      "/boards": { page: "/boards" },
+      "/404": { page: "/404" },
+    });
+  },
 };
 
 module.exports = nextConfig;
--- pages/boards/[boardId]/index.js
+++ pages/boards/[boardId]/index.js
@@ -10,3 +10,7 @@ export default function BoardPage() {
     </>
   );
 }
+// export const getServerSideProps = () => {
+//   // 만약 서버사이드렌더링을 하는 페이지라면 out폴더로 생성불가.
+//   //            ==> 이런경우 next.config.js에서 exportPathMap으로 현재페이지 제외시키기
+// };
